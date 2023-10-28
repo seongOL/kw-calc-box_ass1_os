bbfe9f85640cd82bfe58d9152380e58193ce7dfa
seongOL <145082218+seongOL@users.noreply.github.com>
2023-10-29 00:44:36 +0900

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,39 @@
+function avg(numbers) {
+  let s = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    s += numbers[i];
+  }
+  return s / numbers.length;
+}
+
+function prime(num) {
+  if (num <= 1) {
+    return false;
+  } else if (num % 2 === 0) {
+    return false;
+  }
+
+  for (let i = 3; i < num; i = i + 2) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function fact(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let facto = 1;
+  for (let i = 1; i < num + 1; i++) {
+    facto *= i;
+  }
+  return facto;
+}
+
+module.exports = {
+  avg,
+  prime,
+  fact,
+};
