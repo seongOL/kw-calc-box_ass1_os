840ebe4dee4cd56a6b3a30d4c66adb00a6b2c808
seongOL <145082218+seongOL@users.noreply.github.com>
2023-10-29 01:32:46 +0900

    Add files via upload

--- lib.js
+++ lib.js
@@ -9,6 +9,8 @@ function avg(numbers) {
 function prime(num) {
   if (num <= 1) {
     return false;
+  } else if (num === 2) {
+    return true;
   } else if (num % 2 === 0) {
     return false;
   }
