0b395eee5d4fd0ee6ed7a75091c79d9a8bc1a11f
seongOL <145082218+seongOL@users.noreply.github.com>
2023-10-29 01:04:56 +0900

    Add files via upload

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,11 @@ test("avg([3,5,7]) should be 5", () => {
 test("avg[-5,5]) should be 0", () => {
   expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime[3]) should be true", () => {
+  expect(lib.prime(3)).toBe(true);
+});
+
+test("fact[5]) should be 120", () => {
+  expect(lib.fact(5)).toBe(120);
+});
